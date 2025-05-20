141c649ad50fcaafa7ace199fdcf5fa29937531e
awesomebruh 03 <22234103381@cse.bubt.edu.bd>
2025-05-20 16:10:58 +0000

    fresh workspace

--- src/firebase/firebase.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FirebaseAdminService } from './firebase-admin/firebase-admin.service';
-
-@Module({
-  providers: [FirebaseAdminService]
-})
-export class FirebaseModule {}
